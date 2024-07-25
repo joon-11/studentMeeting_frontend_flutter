4f559cbeb10b739ce3d44e889de16a93d11f59af
joon-11 <123048555+joon-11@users.noreply.github.com>
2024-07-25 19:07:07 +0900

    선생님 소개 창 완성

--- student_meeting/lib/view/screen/profile_detail.dart
+++ student_meeting/lib/view/screen/profile_detail.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -16,17 +17,109 @@ class ProfileDetail extends StatelessWidget {
   }
 }
 
-
 class ProfileListDetail extends StatelessWidget {
   final ProfileModel profile;
+
   const ProfileListDetail({super.key, required this.profile});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('${profile.t_no}')),
+      appBar: AppBar(
+        title: const Text("선생님 소개"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            const Center(
+              child: CircleAvatar(
+                radius: 100,
+                //사진
+                backgroundColor: CupertinoColors.inactiveGray,
+              ),
+            ),
+            const SizedBox(height: 40),
+            Center(
+              child: FractionallySizedBox(
+                widthFactor: 0.9,
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.greenAccent.shade100,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "선생님 정보",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 10),
+                      ListTile(
+                        title: Text(
+                          "이름 - ${profile.t_name}",
+                          style: const TextStyle(fontSize: 20),
+                        ),
+                      ),
+                      ListTile(
+                        title: Text(
+                          "과목 - ${profile.t_lib}",
+                          style: const TextStyle(fontSize: 20),
+                        ),
+                      ),
+                      ListTile(
+                        title: const Text(
+                          "설명",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        subtitle: Text(
+                          "${profile.t_exp}",
+                          style: const TextStyle(
+                              fontSize: 16, color: Colors.black87),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('예약하기 button pressed');
+              },
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all<Color>(
+                    Colors.blue), // Background color
+              ),
+              child: Container(
+                height: 80,
+                width: 300,
+                child: Center(
+                  child: Text(
+                    "${profile.t_lib} 수업 예약하기!",
+                    style: const TextStyle(fontSize: 25, color: Colors.black),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-
-
