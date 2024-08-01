6b846d5c227264c40365c7ff5a134416f11971d7
joon-11 <123048555+joon-11@users.noreply.github.com>
2024-08-01 11:54:22 +0900

    상담일정확인 페이지 완성

--- student_meeting/lib/view/screen/schedule_screen.dart
+++ student_meeting/lib/view/screen/schedule_screen.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:time_scheduler_table/time_scheduler_table.dart';
+import 'package:intl/intl.dart';
 
 import '../../model/reservation_model.dart';
 import '../../viewmodel/reservationViewModel.dart';
@@ -18,28 +20,122 @@ class _ScheduleScreenState extends State<ScheduleScreen> {
   Widget build(BuildContext context) {
     final viewModel = Provider.of<ReservationViewModel>(context);
     print(viewModel.reservationList);
-    return ShowSchdule(
-        schedule: viewModel.reservationList);
+    return ShowSchedule(
+      schedule: viewModel.reservationList,
+    );
   }
 }
 
-class ShowSchdule extends StatefulWidget {
+class ShowSchedule extends StatefulWidget {
   final List<ReservationModel> schedule;
 
-  const ShowSchdule({super.key, required this.schedule});
+  const ShowSchedule({super.key, required this.schedule});
 
   @override
-  State<ShowSchdule> createState() => _ShowSchduleState();
+  State<ShowSchedule> createState() => _ShowScheduleState();
 }
 
-class _ShowSchduleState extends State<ShowSchdule> {
+class _ShowScheduleState extends State<ShowSchedule> {
+  void showPopup(BuildContext context, String title, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              child: Text("확인"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text("수업 취소하기"))
+          ],
+        );
+      },
+    );
+  }
+
+  selectPhoto(lib) {
+    List photo = [
+      'lib/images/루피.webp',
+      'lib/images/조로.webp',
+      'lib/images/나미.webp',
+      'lib/images/우솝.webp',
+      'lib/images/상디.jpeg',
+      'lib/images/쵸파.webp'
+    ];
+
+    if (lib == "국어") {
+      return (photo[0]);
+    } else if (lib == "검술") {
+      return (photo[1]);
+    } else if (lib == "항해술") {
+      return (photo[2]);
+    } else if (lib == "사격") {
+      return (photo[3]);
+    } else if (lib == "요리") {
+      return (photo[4]);
+    } else if (lib == "의학") {
+      return (photo);
+    } else {
+      return (photo[0]);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('내일정'),
+        title: Text('내 일정', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blueAccent,
+        elevation: 0,
       ),
-      // body: ListView.builder(itemBuilder: itemBuilder)
+      body: ListView.builder(
+        itemCount: widget.schedule.length,
+        itemBuilder: (context, index) {
+          final ReservationModel profile = widget.schedule[index];
+          final DateTime dateTime = DateTime.parse(profile.time!);
+          final DateFormat formatter = DateFormat('yyyy-MM-dd HH:mm');
+          final String formattedStartTime = formatter.format(dateTime);
+          final String formattedEndTime =
+              formatter.format(dateTime.add(Duration(hours: 1)));
+
+          return Card(
+            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            child: ListTile(
+              contentPadding: const EdgeInsets.all(16),
+              leading: CircleAvatar(
+                radius: 30,
+                backgroundColor: CupertinoColors.inactiveGray,
+                backgroundImage: AssetImage(selectPhoto(profile.lib)),
+              ),
+              title: Text(
+                '${profile.lib} 수업 시간',
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+              subtitle: Text(
+                '$formattedStartTime - $formattedEndTime',
+                style: TextStyle(
+                  color: Colors.grey[600],
+                ),
+              ),
+              onTap: () {
+                showPopup(context, '${profile.lib} 수업 정보',
+                    '시간: $formattedStartTime - $formattedEndTime');
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 }
